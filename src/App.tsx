8047714f364bd9a8e12c3624435a6a439aa2e084
Josh <jmillikan12345@gmail.com>
2022-01-12 14:29:43 -0500

    testing webgl, made a spinning triangle

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 /** @jsxImportSource @emotion/react */
 import {css} from "@emotion/react";
 import {CodeEditor} from "./CodeEditor";
+import {RenderingEngine} from "./graphics/rendering";
 
 function App() {
     const mainStyle = css`
-      background-color: rgb(26,28,31);
+      background-color: rgb(26, 28, 31);
       height: 100vh;
       width: 100vw;
     `
+
+    useEffect(() => {
+        try {
+            let renderingEngine = new RenderingEngine(45,0.1,100.0)
+            renderingEngine.render(0)
+        } catch (error:any) {
+            alert(`WebGL initialization failed. your browser may not be compatible\nError: ${error.message}`)
+        }
+    },[])
     return (
         <div css={mainStyle}>
             <header
                 css={css`
-                  background-color: rgb(58,60,64);
+                  background-color: rgb(58, 60, 64);
                   text-align: center;
                   user-select: none;
                   height: 5%;
@@ -29,13 +39,16 @@ function App() {
               padding-right: 10%;
               padding-top: 10%;
             `}>
-            <canvas id={"webgl"} css={css`
-              background-color: white;
-              border-width: 2px;
-              border-style: ridge;
-              border-color: rgb(50,50,50);
-              height: 40vh;
-            `}/>
+            <canvas
+                id={"webgl"}
+                css={css`
+                  border-width: 2px;
+                  border-style: ridge;
+                  border-color: rgb(50, 50, 50);
+                `}
+                width={"720"}
+                height={"500"}
+            />
         </span>
         </div>
     );
---
+++ src/graphics/rendering.ts
@@ -0,0 +1,164 @@
+import {mat2, mat3, mat4, vec3} from "gl-matrix";
+
+const vertexShaderSouce = `
+    attribute vec4 aVertexPosition;
+    attribute vec4 aVertexColor;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    varying lowp vec4 vColor;
+
+    void main(void) {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vColor = aVertexColor;
+    }
+  `;
+
+const fragShaderSource = `
+    varying lowp vec4 vColor;
+
+    void main(void) {
+      gl_FragColor = vColor;
+    }
+  `;
+
+interface ProgramData {
+    program: WebGLProgram,
+    vertexPosition: number,
+    vertexColor: number,
+    projection: WebGLUniformLocation,
+    model: WebGLUniformLocation
+}
+
+interface Buffers {
+    position: WebGLBuffer,
+    color: WebGLBuffer
+}
+
+export class RenderingEngine {
+    private gl: WebGLRenderingContext
+    private lastTime: number = 0;
+    private projection = mat4.create()
+    private modelView = mat4.create()
+    private buffers: Buffers
+    private program: ProgramData
+
+    constructor(fov: number, near: number, far: number) {
+        const gl = document.querySelector<HTMLCanvasElement>("#webgl")?.getContext("webgl");
+        this.gl = gl!
+        mat4.perspective(
+            this.projection,
+            fov * Math.PI / 180,
+            this.gl.canvas.clientWidth / this.gl.canvas.clientHeight,
+            near,
+            far
+        )
+        mat4.translate(this.modelView,this.modelView,[-0.2,0,-2])
+        if (gl) {
+            gl.clearColor(0.0, 0.0, 0.0, 1.0)
+            gl.clear(gl.COLOR_BUFFER_BIT)
+            const program = this.loadShaders(vertexShaderSouce, fragShaderSource)
+            this.program = {
+                program: program,
+                vertexPosition: this.gl.getAttribLocation(program, 'aVertexPosition'),
+                vertexColor: this.gl.getAttribLocation(program,"aVertexColor"),
+                projection: this.gl.getUniformLocation(program, "uProjectionMatrix")!,
+                model: this.gl.getUniformLocation(program, "uModelViewMatrix")!
+            }
+            this.buffers = this.initBuffers()
+        } else {
+            throw new Error("WebGL initialization failed")
+        }
+    }
+
+    render(currentTime: number) {
+        currentTime *= 0.001
+        const deltaTime = currentTime - this.lastTime
+        this.lastTime = currentTime
+        this.draw(deltaTime)
+        mat4.rotate(this.modelView,this.modelView,deltaTime,vec3.fromValues(0,0,1))
+        requestAnimationFrame((time) => {
+            this.render(time)
+        })
+    }
+
+    private draw(deltaTime:number) {
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0)
+        this.gl.clearDepth(1.0)
+        this.gl.enable(this.gl.DEPTH_TEST)
+        this.gl.depthFunc(this.gl.LEQUAL)
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT)
+
+        const numComponents = 4;
+        const type = this.gl.FLOAT;
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.position)
+        this.gl.vertexAttribPointer(
+            this.program.vertexPosition,
+            numComponents,
+            type,
+            false,
+            0,
+            0
+        )
+        this.gl.enableVertexAttribArray(this.program.vertexPosition)
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER,this.buffers.color)
+        this.gl.vertexAttribPointer(this.program.vertexColor,numComponents,type,false,0,0)
+        this.gl.enableVertexAttribArray(this.program.vertexColor)
+        this.gl.useProgram(this.program.program)
+        this.gl.uniformMatrix4fv(this.program.projection, false, this.projection)
+        this.gl.uniformMatrix4fv(this.program.model, false, this.modelView)
+
+        const vertexCount = 3
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP,0,vertexCount)
+
+    }
+
+    private initBuffers(): Buffers {
+        const buffer = this.gl.createBuffer()
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer)
+        const positions = [
+            -0.5, -0.5, 0.0, 1.0,
+            0.5, -0.5, 0.0, 1.0,
+            0.0,0.5,0.0, 1.0
+        ];
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW)
+        const colors = [
+            1.0,0.0,0.0, 1.0,
+            0.0,1.0,0.0, 1.0,
+            0.0,0.0,1.0, 1.0
+        ];
+        const colorBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, colorBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(colors), this.gl.STATIC_DRAW);
+
+        return {
+            position: buffer!,
+            color: colorBuffer!
+        }
+    }
+
+    private loadShaders(vertex: string, fragment: string): WebGLProgram {
+        const program = this.gl.createProgram()
+        if (program) {
+            this.gl.attachShader(program, this.compileShader(vertex, this.gl.VERTEX_SHADER))
+            this.gl.attachShader(program, this.compileShader(fragment, this.gl.FRAGMENT_SHADER))
+            this.gl.linkProgram(program)
+            return program
+        } else throw new Error("Failed to create shader program")
+    }
+
+    private compileShader(source: string, type: number): WebGLShader {
+        const shader = this.gl.createShader(type)
+        if (shader) {
+            this.gl.shaderSource(shader, source)
+            this.gl.compileShader(shader)
+            if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+                throw new Error(`Shader compilation failed: ${this.gl.getShaderInfoLog(shader)}`)
+            }
+            return shader
+        } else throw new Error("Shader creation failed")
+    }
+}
+
