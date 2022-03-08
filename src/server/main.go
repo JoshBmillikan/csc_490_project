ce7169dfa4b4cffe2ca4ab276059f032891f888a
Josh <jmillikan12345@gmail.com>
2022-03-08 18:57:37 -0500

    test

--- src/server/main.go
+++ src/server/main.go
@@ -5,5 +5,8 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.Static("/", "./www/")
+	r.GET("/api", func(context *gin.Context) {
+		context.String(200, "You have reached the api root for the csc 490 project server")
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
